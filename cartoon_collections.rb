a912f3474a647a1877fa38feccc20102cbc6eb11
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-18 22:12:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,6 @@ def long_planeteer_calls(elements)
   answer
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
